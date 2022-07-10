d00a4592718b5c5d1a15c2e6cf6b2fbbb290d35e
Stepan0202 <stepan020293@gmail.com>
2022-07-10 22:47:18 +0300

    starting a carousel of comments script

---
+++ public/js/comments-switcher.js
@@ -0,0 +1,10 @@
+let items = document.querySelectorAll('.listener-fb__item');
+let leftButton = document.querySelector('.listener-fb__left-button');
+let rightButton = document.querySelector('.listener-fb__right-button');
+
+leftButton.onclick = moveLeft;
+rightButton.onclick = moveRight;
+
+function moveLeft(){
+    
+}
