8ec55990030f0d73e01df0e84e0ec959a83b85fa
Stepan0202 <stepan020293@gmail.com>
2022-08-07 02:25:33 +0300

    main page is ready

--- public/js/infinity.js
+++ public/js/infinity.js
@@ -1,55 +1,20 @@
-
-let galleryMain = document.querySelector(".gallerey").appendChild(document.querySelector(".gallery__wrapper").cloneNode(true));
-let galleryWrapper = document.querySelectorAll(".gallery__wrapper");
-const galleryMainRect = galleryMain.getBoundingClientRect();
-const galleryMainWidth = galleryMainRect.right - galleryMainRect.left;
-
-
-window.addEventListener('scroll', init,{"once" : true});
-
-//helpfull function to get wright position of element
-function offset(el){
-    const rect = el.getBoundingClientRect(),
-        scrollLeft = window.scrollX || document.documentElement.scrollLeft,
-        scrollTop = window.scrollY || document.documentElement.scrollTop;
-    return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
-
+let galleryWrapper = document.getElementsByClassName("gallery__wrapper");
+let galleryItems = document.getElementsByClassName("gallery__item");
+const galleryItemWidth = galleryItems[0].getBoundingClientRect().width;
+let itemNumber = 0;
+galleryWrapper[0].style.left = 0 + "px";
+setInterval(()=>{
+    if(itemNumber > (galleryItems.length-1)){
+        galleryWrapper[0].style.left = 392 + "px";
+        itemNumber = 0;
     }
+    console.log("setInterval: " + itemNumber);
+    galleryWrapper[0].style.left = (galleryWrapper[0].style.left.split("px")[0]-galleryItemWidth-20) + "px";
+    itemNumber++;
 
-function init(){
-    let n1 = 1;
-    let n2 = 1;
-    let controllN = 0;
-    setInterval(()=>{
-        console.log("START init func. controllN = " + controllN);
-        console.log("START init func. galleryMainWidth = " + galleryMainWidth);
-        galleryWrapper[0].style.left = -n1 + "%";
-        galleryWrapper[1].style.left = -n2 + "%";
-        
-        console.log( "0 element left: " + offset(galleryWrapper[0]).left);
-        console.log( "1 element left: " + offset(galleryWrapper[1]).left);
-        n1++;
-        n2++;
-        if(offset(galleryWrapper[controllN]).left < -(galleryMainWidth/1.8)){
-            if(controllN == 0){
-                console.log("0: Math.abs(offset(galleryWrapper[controllN]).left) = " + Math.abs(offset(galleryWrapper[controllN]).left));
-                galleryWrapper[controllN].style.opacity = 0;
-                galleryWrapper[controllN].removeAttribute("style");
-                n1 = -62;
-                controllN = 1;
-                galleryWrapper[0].style.opacity = 100;
-            }
-            else {
-                console.log("CHANGE in 1");
-                galleryWrapper[controllN].style.opacity = 0;
-                galleryWrapper[controllN].removeAttribute("style");
-                n2 = -62;
-                controllN = 0;
-                galleryWrapper[1].style.opacity = 100;
-            }
-        }
+},2000);
 
-    }, 100)
-}
+    
+
 
 
